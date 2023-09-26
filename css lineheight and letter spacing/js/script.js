0e55819cd4844fb3d78fdf1d0e9749949829f8c4
Saikat Das <saikatdas40g@gmail.com>
2023-09-26 20:37:43 +0530

    added new

--- css lineheight and letter spacing/js/script.js
+++ css lineheight and letter spacing/js/script.js
@@ -1,9 +1,13 @@
 const lineheight = document.querySelector('#lineheight');
 const letterspacing = document.querySelector('#letterspacing');
-const text = document.querySelector('.text')
+const text = document.querySelector('.text');
+const lineheightcss = document.querySelector('.lineheightcss');
+const letterspacingcss = document.querySelector('.letterspacingcss');
 lineheight.addEventListener('input', () => {
     text.style.lineHeight = `${lineheight.value / 10}`;
+    lineheightcss.innerHTML = `${lineheight.value/ 10}`;
 })
 letterspacing.addEventListener('input', () => {
     text.style.letterSpacing = `${letterspacing.value}px`;
+    letterspacingcss.innerHTML = `${letterspacing.value}`;
 })
