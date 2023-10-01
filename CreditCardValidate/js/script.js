2c3acc05849dc8112670a48ec5ea2687c8fb64dd
zainbinshakoor <yammybajwa09@gmail.com>
2023-10-01 15:09:43 -0700

    feat:credit-card-validate

---
+++ CreditCardValidate/js/script.js
@@ -0,0 +1,34 @@
+function validateCreditCard() {
+    const cardNumber = document.getElementById('cardNumber').value;
+    const resultElement = document.getElementById('result');
+    
+    // Remove spaces and non-digit characters
+    const cleanedCardNumber = cardNumber.replace(/\D/g, '');
+    
+    // Check if the card number is a valid format using regex
+    const cardPattern = /^(?:3[47]\d{13}|(?:4\d|5[1-5])\d{14}|6011\d{12}|(?:2131|1800)\d{11})$/;
+    
+    if (!cardPattern.test(cleanedCardNumber)) {
+        resultElement.textContent = 'Invalid credit card number!';
+        resultElement.className = 'invalid';
+        return; // Exit early if the format is invalid
+    }
+    
+    // Apply the Luhn algorithm
+    const cardArray = cleanedCardNumber.split('').reverse().map(Number);
+    const sum = cardArray.reduce(function (acc, digit, index) {
+        if (index % 2 === 1) {
+            const doubledDigit = digit * 2;
+            return acc + (doubledDigit > 9 ? doubledDigit - 9 : doubledDigit);
+        }
+        return acc + digit;
+    }, 0);
+
+    if (sum % 10 === 0) {
+        resultElement.textContent = 'Valid credit card number!';
+        resultElement.className = 'valid';
+    } else {
+        resultElement.textContent = 'Invalid credit card number!';
+        resultElement.className = 'invalid';
+    }
+}
