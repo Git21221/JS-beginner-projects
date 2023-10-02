3b6c1c531168e0c4768df24d8e334c4fccd73196
Mayur <mayurrokade1718@gmail.com>
2023-10-02 12:26:34 +0530

    Dark mode added

--- BMI calculator/js/script.js
+++ BMI calculator/js/script.js
@@ -1,16 +1,29 @@
 const form = document.querySelector("form");
-    const calc = document.querySelector("button");
-    form.addEventListener('submit', (e) => {
-        e.preventDefault();
-        const height = parseFloat(document.querySelector("#height").value);
-        const weight = parseFloat(document.querySelector("#weight").value);
-        const result = document.querySelector("#result");
-        if (height < 0 || height === '' || isNaN(height)) result.innerHTML = "Please input a correct value";
-        else if (weight < 0 || weight === '' || isNaN(weight)) result.innerHTML = "Please input a correct value";
-        else {
-            let bmi = (weight / (height * height)).toFixed(2);
-            if (bmi >= 18.6 && bmi <= 24.9) result.innerHTML = `Normal and BMI ${bmi}`;
-            if (bmi < 18.6) result.innerHTML = `UnderWeight and BMI ${bmi}`;
-            if (bmi > 24.9) result.innerHTML = `OverWeight and BMI ${bmi}`;
-        }
-    })
+const calc = document.querySelector("button");
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const height = parseFloat(document.querySelector("#height").value); //value will take after entering int
+    const weight = parseFloat(document.querySelector("#weight").value);
+    const result = document.querySelector("#result");
+    if(height < 0 || height === '' || isNaN(height)) result.innerHTML = "Please input a correct value";
+    else if(weight < 0 || weight === '' || isNaN(weight)) result.innerHTML = "Please input a correct value";
+    else{
+        let bmi =  (weight / (height * height)).toFixed(2);
+        if(bmi >= 18.6 && bmi <= 24.9) result.innerHTML = `Normal and BMI ${bmi}`;
+        if(bmi < 18.6) result.innerHTML = `UnderWeight and BMI ${bmi}`;
+        if(bmi > 24.9) result.innerHTML = `Motu and BMI ${bmi}`;
+    }
+})
+
+const toggleDarkModeButton = document.getElementById("toggleDarkMode");
+const stylesheet = document.getElementById("stylesheet");
+
+toggleDarkModeButton.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+    // Toggle the stylesheet between light and dark modes
+    if (stylesheet.getAttribute("href") === "css/style.css") {
+        stylesheet.setAttribute("href", "css/dark-style.css");
+    } else {
+        stylesheet.setAttribute("href", "css/style.css");
+    }
+});
