f518169903988faead3a7f194d6d490ef1b046ce
ABHISHEK KUMAR <abhishekkumar@ABHISHEKs-MacBook-Air.local>
2023-10-03 17:06:45 +0530

    feat: Enable mouse click interaction and add key press styling to drum kit
    This commit allows users to trigger sounds by clicking drum buttons, expanding interaction beyond keyboard keys. Implemented visual enhancement: when a drum key is pressed, its border changes to white, providing immediate feedback to the user.

--- DrumKit/main.js
+++ DrumKit/main.js
@@ -1,18 +1,35 @@
-function playSound(n) {
-  const audio = document.querySelector(`audio[data-key="${n}"]`);
-  const key = document.querySelector(`.key[data-key="${n}"]`);
+
+function playSound(keyCode) {
+  const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
+  const key = document.querySelector(`button[data-key="${keyCode}"]`);
+
+  if (!audio) return; 
+
   key.classList.add("clicked");
+  audio.currentTime = 0;
+  audio.play().catch(error => {
+    console.error("Error playing audio: ", error.message);
+  });
 
   setTimeout(() => {
     key.classList.remove("clicked");
   }, 300);
-
-  if (!audio) return;
-  audio.currentTime = 0;
-  audio.play();
 }
 
-window.document.addEventListener("keydown", (e) => {
-  console.log(e.key, e.key.toUpperCase().charCodeAt(0));
-  playSound(e.key.toUpperCase().charCodeAt(0));
+function handleKeyOrMouseClick(event) {
+  let keyCode;
+
+  if (event.type === "keydown") {
+    keyCode = event.keyCode;
+  } else if (event.type === "click") {
+    keyCode = parseInt(event.target.getAttribute("data-key"));
+  }
+
+  playSound(keyCode);
+}
+
+window.addEventListener("keydown", handleKeyOrMouseClick);
+const keys = document.querySelectorAll(".key");
+keys.forEach(key => {
+  key.addEventListener("click", handleKeyOrMouseClick);
 });
